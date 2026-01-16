d355078556b8534c41daf3ccfeff9159619c29c5
Peter Berck <peter.berck@humlab.lu.se>
2026-01-16 11:03:18 +0100

    Added chunk_file_prefix() and test/data.

--- src/embedder.rs
+++ src/embedder.rs
@@ -1,6 +1,8 @@
 use fastembed::{Embedding, EmbeddingModel, InitOptions, TextEmbedding};
 use std::fs;
 use std::fs::read_dir;
+use std::fs::File;
+use std::io::{self, BufRead};
 use std::path::Path;
 use std::path::PathBuf;
 use text_splitter::TextSplitter;
@@ -32,6 +34,8 @@ pub fn chunk_string(text: &str, max_len: usize) -> Vec<String> {
     splitter.chunks(text).map(|v| v.to_string()).collect()
 }
 
+// Does this function make sense? We woud need to feed it each line in one
+// of the "prepared" documents, prefix is the first part, the text the rest.
 pub fn chunk_string_prefix(text: &str, prefix: &str, max_len: usize) -> (Vec<String>, Vec<String>) {
     let max_characters = max_len - 25..max_len + 25;
     let splitter = TextSplitter::new(max_characters);
@@ -78,6 +82,37 @@ pub fn chunk_file_txt<P: AsRef<Path>>(path: P, chunk_size: usize) -> anyhow::Res
     Ok(chunk_string(&contents, chunk_size))
 }
 
+pub fn chunk_file_prefix_txt<P: AsRef<Path>>(
+    path: P,
+    chunk_size: usize,
+) -> anyhow::Result<(Vec<String>, Vec<String>)> {
+    let file = File::open(&path)?;
+    let mut col1 = Vec::new();
+    let mut col2 = Vec::new();
+
+    if let lines = io::BufReader::new(file).lines() {
+        for line in lines {
+            if let Ok(content) = line {
+                // Arbitrary limit...
+                if content.len() <= 12 {
+                    continue;
+                }
+                let mut parts = content.splitn(3, '\t');
+                if let (Some(a), Some(b)) = (parts.next(), parts.next()) {
+                    // trace!("{}\t{}", a, b);
+                    let (prefixes, chunks) = chunk_string_prefix(b, a, chunk_size); // text, prefix, len
+                    col1.extend(prefixes);
+                    col2.extend(chunks);
+                }
+            }
+        }
+    } else {
+        eprintln!("Could not read file: {}", path.as_ref().display());
+    }
+
+    Ok((col1, col2))
+}
+
 // This one also only chunks.
 pub fn chunk_file_pdf<P: AsRef<Path>>(path: P, chunk_size: usize) -> anyhow::Result<Vec<String>> {
     let bytes = std::fs::read(path).unwrap();
@@ -112,6 +147,16 @@ pub fn get_embedding_dim() -> anyhow::Result<usize> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::path::{Path, PathBuf};
+
+    fn make_test_path(file_name: &str) -> PathBuf {
+        let project_root = env!("CARGO_MANIFEST_DIR");
+        let data_path = Path::new(project_root)
+            .join("assets")
+            // .join("data")
+            .join(file_name);
+        data_path
+    }
 
     #[test]
     fn chunk_ml100() {
@@ -218,6 +263,19 @@ mod tests {
         assert!(
             results[0]
                 == "the quick brown fox jumps over the lazy dog. And another sentence. Seven!"
+        );
+    }
+
+    #[test]
+    fn chunk_prefix_file() {
+        let data_path = make_test_path("extras_prepared.txt");
+        let (v1, v2) = chunk_file_prefix_txt(data_path, 128).expect("No file");
+        dbg!("{:?}", &v1);
+        dbg!("{:?}", &v2);
+        assert!(v1[1] == "BOOK EXTRA/CHAPTER STANFORD/Encyclopedia 1");
+        assert!(
+            v2[1]
+                == "Your approach was secular, non-metaphysical, and anti-authoritarian; it eschewed religious appeals, scholastic"
         );
     }
 }
